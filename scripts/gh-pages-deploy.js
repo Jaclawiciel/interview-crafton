b8238a89e8567b2a633ea8851a08f36f69f8fb85
Jacek Gałka <jacgalka@icloud.com>
2021-04-05 17:34:40 +0200

    Automate deploy

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,8 +1,21 @@
 const execa = require("execa");
 const fs = require("fs");
+import {v4 as uuidv4} from 'uuid'
+
+
 
 (async () => {
     try {
+        // disable workflow commands
+        const token = uuidv4()
+        console.log(`::stop-commands::${token}`)
+
+// log untrusted output
+        console.log("untrusted output")
+
+// enable workflow commands
+        console.log(`::${token}::`)
+
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         console.log("Building...");
         await execa("npm", ["run", "build"]);
